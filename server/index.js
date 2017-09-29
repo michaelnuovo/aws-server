fbba7eb1effbfda2bf517e5903231af0c09ae7bf
Michael Casalinuovo <michael.nuovo@gmail.com>
2017-09-29 11:33:58 -0700

    server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+var net = require('net');
+
+var tcpServer = net.createServer(function(socket) {
+
+        console.log('connection established...')
+
+        socket.on('end',function(){
+            console.log('server disconnected...')
+        })
+
+	socket.on('data',function(data){
+            console.log('data received from the tcp client')
+                console.log('data.length = '+data.length)
+                for( i =0;i<data.length;i++){
+                        console.log('data[] = '+data[i])
+                        }
+            socket.write('Server Reply: '+data)
+        })
+});
